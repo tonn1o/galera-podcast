cfd2e7e6de28cd637aa2f7ee6b61eaa656f0358e
Anton Babenko <anton.babenko@ac-project.net>
2018-12-02 19:08:53 +0200

    update project

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,228 @@
-require('webpack');
-const WebpackBar = require('webpackbar');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const processHTMLPages = require('./processHTMLHelper.js');
+const path = require('path');
+const webpack = require('webpack');
+const minJSON = require('jsonminify');
+const Dotenv = require('dotenv-webpack');
 
-const extractCSS = new ExtractTextPlugin('style.css');
-const ProgressBar = new WebpackBar();
-const plugins = [
-  ProgressBar,
-  extractCSS,
-].concat(processHTMLPages());
+const plugins = {
+    progress: require('webpackbar'),
+    clean: require('clean-webpack-plugin'),
+    extractCSS: require('mini-css-extract-plugin'),
+    sync: require('browser-sync-webpack-plugin'),
+    html: require('html-webpack-plugin'),
+    copy: require('copy-webpack-plugin'),
+    sri: require('webpack-subresource-integrity')
+};
 
-module.exports = {
-  entry: [
-    'webpack-dev-server/client?http://localhost:8080',
-    './source/index.js'
-  ],
-  module: {
-    rules: [
-      {
-        test: [/\.scss$/i, /\.sass$/i, /\.css$/],
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'postcss-loader', 'sass-loader']
-        })
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader',
-        query: {
-          name: '[path][name].[ext]',
-          context: './source'
-        }
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.es6'],
-  },
-  output: {
-    path: '/build',
-    filename: 'index.js',
-  },
-  devServer: {
-    contentBase: './source'
-  },
-  plugins,
+module.exports = (env = {}, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    let config = {
+        context: path.resolve(__dirname, 'src'),
+
+        entry: {
+            app: [
+                './styles/app.sass',
+                './scripts/app.js'
+            ]
+        },
+
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: '',
+            filename: 'scripts/[name].js',
+            crossOriginLoading: 'anonymous'
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.((s[ac]|c)ss)$/,
+                    use: [
+                        plugins.extractCSS.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: ! isProduction
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                ident: 'postcss',
+                                sourceMap: ! isProduction,
+                                plugins: (() => [
+                                    require('autoprefixer')(),
+                                    ...isProduction ? [
+                                        require('cssnano')({
+                                            preset: ['default', {
+                                                minifySelectors: false
+                                            }]
+                                        })
+                                    ] : []
+                                ])
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                outputStyle: 'expanded',
+                                sourceMap: ! isProduction
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-env'
+                            ]
+                        }
+                    }
+                },
+                {
+                    test: /\.(gif|png|jpe?g|svg)$/i,
+                    exclude: /fonts/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[path][name].[ext]',
+                                publicPath: '..' // use relative urls
+                            }
+                        },
+                        {
+                            loader: 'image-webpack-loader',
+                            options: {
+                                bypassOnDebug: ! isProduction,
+                                mozjpeg: {
+                                    progressive: true,
+                                    quality: 65
+                                },
+                                optipng: {
+                                    enabled: false
+                                },
+                                pngquant: {
+                                    quality: '65-90',
+                                    speed: 4
+                                },
+                                gifsicle: {
+                                    interlaced: false
+                                }
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                    exclude: /images/,
+                    use: [{
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/',
+                            publicPath: '../fonts/' // use relative urls
+                        }
+                    }]
+                },
+                {
+                    test: /\.html$/,
+                    use: {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: true,
+                            removeComments: true,
+                            collapseWhitespace: true,
+                            removeScriptTypeAttributes: true,
+                            removeStyleTypeAttributes: true
+                        }
+                    },
+                }
+            ]
+        },
+
+        devServer: {
+            contentBase: path.join(__dirname, 'src'),
+            port: 8080,
+            overlay: {
+                warnings: true,
+                errors: true
+            },
+            quiet: true
+        },
+
+        plugins: (() => {
+            let common = [
+                new Dotenv(),
+                new plugins.extractCSS({
+                    filename: 'styles/[name].css'
+                }),
+                new plugins.html({
+                    template: 'index.ejs',
+                    filename: 'index.html',
+                    minify: {
+                        removeScriptTypeAttributes: true,
+                        removeStyleLinkTypeAttributes: true
+                    }
+                }),
+                new plugins.progress({
+                    color: '#5C95EE'
+                })
+            ];
+
+            const production = [
+                new plugins.clean(['dist']),
+                new plugins.copy([
+                    {
+                        from: 'data/**/*.json',
+                        // to: '',
+                        transform: content => {
+                            return minJSON(content.toString())
+                        }
+                    }
+                ]),
+                new plugins.sri({
+                    hashFuncNames: ['sha384'],
+                    enabled: true
+                })
+            ];
+
+            const development = [
+                new plugins.sync(
+                    {
+                        host: 'localhost',
+                        port: 9090,
+                        proxy: 'http://localhost:8080/'
+                    },
+                    {
+                        reload: false
+                    }
+                )
+            ];
+
+            return isProduction
+                ? common.concat(production)
+                : common.concat(development)
+        })(),
+
+        devtool: (() => {
+            return isProduction
+                ? '' // 'hidden-source-map'
+                : 'source-map'
+        })(),
+
+        resolve: {
+            modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+            alias: {
+                '~': path.resolve(__dirname, 'src/scripts/')
+            }
+        },
+
+        stats: 'errors-only'
+    };
+
+    return config
 };
